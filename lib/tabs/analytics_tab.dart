a4cdb2cedfbcdce846e37fb762f9bd1b01303fa6
Ole <ole.fjeldsaa@live.com>
2022-05-04 15:45:31 +0200

    Changed temp_tab to analytics_tab

--- lib/tabs/analytics_tab.dart
+++ lib/tabs/analytics_tab.dart
@@ -2,22 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:elli_admin/firebase_handler.dart';
 import '../models/space.dart';
 
-class TempTab extends StatefulWidget {
-  const TempTab({Key? key}) : super(key: key);
+class AnalyticsTab extends StatefulWidget {
+  const AnalyticsTab({Key? key}) : super(key: key);
 
   @override
-  State<TempTab> createState() => _TempTabState();
+  State<AnalyticsTab> createState() => _AnalyticsTabState();
 }
 
 //Widget for selecting office, picking day, picking room and then booking a timeslot
-class _TempTabState extends State<TempTab> {
+class _AnalyticsTabState extends State<AnalyticsTab> {
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: const <Widget>[
         SizedBox(height: 10),
-        Text("Temp",
+        Text("Analytics",
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 30,
@@ -26,4 +26,3 @@ class _TempTabState extends State<TempTab> {
     );
   }
 }
-
