41129223cf5e3866a89411ff018f2e10b96d7adf
Ole <ole.fjeldsaa@live.com>
2022-05-06 10:54:51 +0200

    Added cards to config tab

--- lib/tabs/config_tab.dart
+++ lib/tabs/config_tab.dart
@@ -13,16 +13,73 @@ class ConfigTab extends StatefulWidget {
 class _ConfigTabState extends State<ConfigTab> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: const <Widget>[
-        SizedBox(height: 10),
-        Text("    Config",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-            )),
-      ],
+    return SingleChildScrollView(
+      /// The "Analytics" header
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 10),
+            const Text("    Config",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                )),
+            const SizedBox(height: 24),
+
+            /// The card section with all the items under each dropdown menu
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(width: 50),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildCard("FAQ", "edit"),
+                      _buildCard("Add admin", "edit"),
+                      _buildCard("About", "edit")
+                    ],
+                  ),
+                ),
+                Container(width: 50)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(String header, String subtitle) {
+    return Container(
+      width: 400,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 6, 0, 6),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    header,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(subtitle),
+                  const SizedBox(height: 4),
+                ],
+              )),
+          color: Colors.grey.shade100,
+        ),
+      ),
     );
   }
 }
