ec62d488dccb982a34b200019684487a529d6b64
Ole <ole.fjeldsaa@live.com>
2022-05-16 10:47:05 +0200

    Removed FAQ from config tab and made started adding proper documentation.

--- lib/tabs/config_tab.dart
+++ lib/tabs/config_tab.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:elli_admin/firebase_handler.dart';
-import '../models/space.dart';
 
 /// A tab for viewing the admin config settings for ELLI
 ///
@@ -57,142 +56,9 @@ class _ConfigTabState extends State<ConfigTab> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(
-                        width: 400,
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 6, 0, 6),
-                          child: Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(4.0),
-                            ),
-                            child: Padding(
-                                padding: const EdgeInsets.all(16),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      "FAQ",
-                                      style: TextStyle(fontSize: 16),
-                                    ),
-                                    //This is where the edit button lives
-                                    SizedBox(
-                                      width: 120,
-                                      child: TextButton(
-                                        onPressed: () {
-                                          showDialog(
-                                              context: context,
-                                              builder: (BuildContext context) =>
-                                                  _buildPopupDialog(
-                                                      context,
-                                                      "FAQ",
-                                                      "Add possibility to edit FAQ here"));
-                                        },
-                                        child: const Align(
-                                          alignment: Alignment.center,
-                                          child: Text(
-                                            'edit',
-                                            style: TextStyle(
-                                              color: Colors.grey,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                )),
-                            color: Colors.grey.shade100,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 400,
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 6, 0, 6),
-                          child: Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(4.0),
-                            ),
-                            child: Padding(
-                                padding: const EdgeInsets.all(16),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      "Admins",
-                                      style: TextStyle(fontSize: 16),
-                                    ),
-                                    SizedBox(
-                                      width: 120,
-                                      child: TextButton(
-                                        onPressed: () {
-                                          showDialog(
-                                              context: context,
-                                              builder: (BuildContext context) =>
-                                                  _buildAdminEdit(context));
-                                        },
-                                        child: const Align(
-                                          alignment: Alignment.center,
-                                          child: Text(
-                                            'edit',
-                                            style: TextStyle(
-                                              color: Colors.grey,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                )),
-                            color: Colors.grey.shade100,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 400,
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 6, 0, 6),
-                          child: Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(4.0),
-                            ),
-                            child: Padding(
-                                padding: const EdgeInsets.all(16),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      "About",
-                                      style: TextStyle(fontSize: 16),
-                                    ),
-                                    SizedBox(
-                                      width: 120,
-                                      child: TextButton(
-                                        onPressed: () {
-                                          showDialog(
-                                              context: context,
-                                              builder: (BuildContext context) =>
-                                                  _buildPopupDialog(
-                                                      context,
-                                                      "About",
-                                                      "Lorem ipsum dolor sit amet"));
-                                        },
-                                        child: const Align(
-                                          alignment: Alignment.center,
-                                          child: Text(
-                                            'have a gander',
-                                            style: TextStyle(
-                                              color: Colors.grey,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                )),
-                            color: Colors.grey.shade100,
-                          ),
-                        ),
-                      ),
+                      //Build the admin card.
+                      _buildAdminCard(context),
+                      _buildAboutCard(context)
                     ],
                   ),
                 ),
@@ -205,6 +71,102 @@ class _ConfigTabState extends State<ConfigTab> {
     );
   }
 
+  Widget _buildAdminCard(BuildContext context) {
+    return SizedBox(
+      width: 400,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 6, 0, 6),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Admins",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  //The button for editing admins
+                  SizedBox(
+                    width: 120,
+                    child: TextButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) =>
+                                _buildAdminEdit(context));
+                      },
+                      child: const Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'edit',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+          color: Colors.grey.shade100,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAboutCard(BuildContext context) {
+    return SizedBox(
+      width: 400,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 6, 0, 6),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "About",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(
+                    width: 120,
+                    child: TextButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) =>
+                                _buildPopupDialog(context, "About",
+                                    "Lorem ipsum dolor sit amet"));
+                      },
+                      child: const Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'have a gander',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+          color: Colors.grey.shade100,
+        ),
+      ),
+    );
+  }
+
+  ///Builds a general template pop up dialog box with a title,
+  ///and one string of content.
   Widget _buildPopupDialog(BuildContext context, String title, String text) {
     return AlertDialog(
       title: Text(title),
@@ -274,16 +236,14 @@ class _ConfigTabState extends State<ConfigTab> {
                                   DropdownButton(
                                     items: adminItems,
                                     onChanged: (admin) {
-                                      final snackBar = SnackBar(
-                                        content: Text(
-                                          'You have selected $admin',
-                                        ),
-                                      );
-                                      Scaffold.of(context)
-                                          .showSnackBar(snackBar);
                                       setState(() {
                                         selectedAdmin = admin;
                                       });
+                                      Navigator.of(context).pop();
+                                      showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) =>
+                                              _buildAdminEdit(context));
                                     },
                                     value: selectedAdmin,
                                     isExpanded: false,
@@ -312,6 +272,7 @@ class _ConfigTabState extends State<ConfigTab> {
       actions: <Widget>[
         TextButton(
           onPressed: () {
+            selectedAdmin = null;
             Navigator.of(context).pop();
           },
           child: const Text('Close'),
